fccb102e733b850c8be831916d42b45b32afe1c3
ganyguru <cprganesh@hotmail.com>
2015-01-12 17:19:56 +0530

    this is the second commit

--- NITT/index.php
+++ NITT/index.php
@@ -5,9 +5,12 @@
 </head>
 <body>
 	<div class ="wrapper">
-		
+		<div class="tbar">
+			<a href="">Placements</a> | <a href="">Library</a> | <a href="">Departments</a> |
+			<span style="float:right;"><a href="">Search</a> | <a href="">WebMail</a> | <a href="">Sitemap</a> | <a href="">Dashboard</a> </span>
+		</div>
 		<div class="htitle">
-			<center><img src="/NITT/img/header.png"/></center>
+			
 		</div>
 		<div class="hmenu ease">
 		<center><ul>
@@ -20,38 +23,94 @@
 		<li class="ease">Contact</li>
 		</ul></center>
 		</div>
-		<div class="eve">
-			<a href="http://www.pragyan.org" target="_blank"><div class="evedes ease">
-				
-				<big style="color:990000;">PRAGYAN 2015</big>
-				<br>Let's Celebrate Technology with pragyan 2015<sup>TM</sup><br>
-				<span style="font-size:12px;color:990000;float:right;padding-right:20px;">READ MORE >></span>
-			</div></a>
-		</div>
-		<div class="content">
-			<div class="side"></div>
-			<div class="scontent">
-				asdASD<br>
-asdASD<br>
-asdASD<br>
-asdASD<br>
-asdASD<br>
-asdASD<br>
-asdASD<br>
+		
+		
+
+		<center>
+			<div class="mcon">
+				<div class="bar">
+				<div class="vision">
+					<center>Vision</center>
+					<p>To provide valuable resources for industry and society through excellence in technical education and research.</p>
+					
+					</div>
+					<div class="mission">
+					<center>Mission<center>
+					<hr>
+					<p>
+					
+					To offer state-of-the-art undergraduate, postgraduate and doctoral programmes.</p><hr><p>
+					To generate new knowledge by engaging in cutting-edge research.</p><hr><p>
+					To undertake collaborative projects with academia and industries.</p><hr><p>
+					To develop human intellectual capability to its fullest potential.</p>
+				</div>
+				</div>
+
+				<div class="bbar">
+				<div class="story">
+				<p class="seading ease">Read Top News ></p><hr>
+				<div class="bar">
+					<div class="news bborder">
+					<p class="gbar">Founder's Desk</p>
+<p class="ibar">We have extensive experience designing interiors for restaurants and offices. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dolor sit amet.</p>
+					</div>
+				</div>
+				<div class="bar">
+					<div class="news bborder">
+					<p class="gbar">Founder's Desk</p>
+<p class="ibar">We have extensive experience designing interiors for restaurants and offices. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dolor sit amet.</p>
+					</div>
+				</div><br>
+				<div class="bar">
+					<div class="news bborder">
+					<p class="gbar">Founder's Desk</p>
+<p class="ibar">We have extensive experience designing interiors for restaurants and offices. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dolor sit amet.</p>
+					</div>
+				</div>
+				<div class="bar">
+					<div class="news bborder">
+					<p class="gbar">Founder's Desk</p>
+<p class="ibar">We have extensive experience designing interiors for restaurants and offices. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dolor sit amet.</p>
+					</div>
+				</div>
+				</div>
+				</div>
+
 			</div>
+			<div class="scon">
 
-		</div>
+				<div class="bar">
+					<p class="heading orange">NOTICES/DOWNLOADS </p><hr>
+					<p class="normal link ">UG/PG : Answer scripts revaluation and Course registration </p><hr>
+					<p class="normal link ">UG/PG : Answer scripts revaluation and Course registration </p><hr>
+					<p class="normal link ">UG/PG : Answer scripts revaluation and Course registration </p><hr>
+					<p class="normal link ">UG/PG : Answer scripts revaluation and Course registration </p><hr>
+					<p class="seading ease">More Notices ></p>
 
-		<div id="footer" class="footer">
-<ul>
-<li>
-	<a href=""><img src="/FBN/img/fico.png"></a>
-	</li><li><a href=""><img src="/FBN/img/gico.png"></a></li><li><a href=""><img src="/FBN/img/tico.png"></a></li></ul>
-</div>
-<div  class="pfooter"></div>
+				</div>
 
+				<div class="bar">
+				<p class="heading orange">EVENTS </p><hr>
+					<div class="din normal  "> <div class="date"><div>JAN.</div>15.</div>  <span class="link" style="margin-left:10px;"> MDP on Knowledge Management & Innovation, 29-31 January 2015 > </span></div><hr>
+					<div class="din normal  "> <div class="date"><div>JAN.</div>15.</div>  <span class="link" style="margin-left:10px;"> MDP on Knowledge Management & Innovation, 29-31 January 2015 > </span></div><hr>
+					<div class="din normal  "> <div class="date"><div>JAN.</div>15.</div>  <span class="link" style="margin-left:10px;"> MDP on Knowledge Management & Innovation, 29-31 January 2015 > </span></div><hr>
+					<p class="seading ease">More Events ></p>
 
-</div>
+				</div>
+
+				<div class="bar">
+				<p class="heading orange">WELCOME </p><hr>
+					<p class="normal link ">COMPUTER CENTER <span style="float:right">></span></p><hr>
+					<p class="normal link ">TEQIP II <span style="float:right">></span></p><hr>
+					<p class="normal link ">SPORTS CENTER <span style="float:right">></span></p><hr>
+					<p class="normal link ">LIBRARY <span style="float:right">></span></p><hr>	
+				</div>
+
+			</div>
+			<div class="pfooter"
+		</center>
+
 	</div>
+
 </body>
 </html>
